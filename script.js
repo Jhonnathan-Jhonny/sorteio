0f993fe27eb2e70f4f13952dea0f6dbc0fec0cef
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2023-08-18 15:30:07 -0300

    up script

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 function sortear() {
-    let inicio = document.getElementById('primeiroNumero').value
-    let fim = document.getElementById('segundoNumero').value
+    let inicio = parseInt(document.getElementById('primeiroNumero').value)
+    let fim = parseInt(document.getElementById('segundoNumero').value)
+    let quantidadeNumeros = parseInt(document.getElementById('quantidadeNumero').value)
 
-    if(inicio > fim){
-        window.alert('Valor não permitido!')
-        return
-    }
-    else if(inicio === "" || inicio === null || fim === "" || fim === null) {
+    if(isNaN(inicio) || isNaN(fim)) {
         window.alert('Valor não identificado!')
         return
     }
-    
+
+    if(inicio > fim){
+    window.alert('Valor não permitido!')
+    return
+    }
+        
+    let resultado = []
     let tamanho = fim - inicio + 1
-    let resultado = Math.floor(Math.random() * tamanho) 
-    window.alert(fim - resultado)
+    for(let i = 0; i < quantidadeNumeros ;i++){
+        resultado[i] = Math.floor(Math.random() * tamanho) 
+    }
+    for(let i = 0; i < quantidadeNumeros ;i++){
+       window.alert(fim - resultado[i]) 
+    }
+}
+
+function plural(){
+    let plural = document.getElementById('sorteio')
+    if(quantidadeNumeros > 1){
+        plural.innerHTML = 'Sortear números'
+    }
 }
